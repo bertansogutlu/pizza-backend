8fb82d398294523bd0069c8d639d8f62247e2cd8
Bertan Sogutlu <bertansogutlu@gmail.com>
2023-04-11 22:05:20 +0300

    routerlarin uc nokta taslaklari olusturuldu

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,7 +1,27 @@
 const router = require('express').Router();
 
-router.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
+router.post('/register', (req, res, next) => {
+  try {
+      res.status(200).send('Register')
+  } catch (error) {
+      next(error)
+  }
+})
+
+router.post('/login', (req, res, next) => {
+  try {
+      res.status(200).send('Login')
+  } catch (error) {
+      next(error)
+  }
+})
+
+router.post('/password', (req, res, next) => {
+  try {
+      res.status(200).send('Password')
+  } catch (error) {
+      next(error)
+  }
+})
 
 module.exports = router;
--- api/pizzas/pizzas-router.js
+++ api/pizzas/pizzas-router.js
@@ -1,7 +1,43 @@
 const router = require('express').Router();
 
-router.get('/', (req, res) => {
-    res.send('Hello World!')
+router.get('/', (req, res, next) => {
+    try {
+        res.status(200).send('Get all')
+    } catch (error) {
+        next(error)
+    }
+  })
+
+  router.get('/:id', (req, res, next) => {
+    try {
+        res.status(200).send('Get by id')
+    } catch (error) {
+        next(error)
+    }
+  })
+
+  router.post('/', (req, res, next) => {
+    try {
+        res.status(201).send('Create')
+    } catch (error) {
+        next(error)
+    }
+  })
+
+  router.put('/', (req, res, next) => {
+    try {
+        res.status(200).send('Update')
+    } catch (error) {
+        next(error)
+    }
+  })
+
+  router.delete('/', (req, res, next) => {
+    try {
+        res.status(200).send('Delete')
+    } catch (error) {
+        next(error)
+    }
   })
 
 module.exports = router;
