73a38cb036df31b020ade6dfcb91d5bc934035e1
Bertan Sogutlu <bertansogutlu@gmail.com>
2023-04-11 21:41:03 +0300

    routerlar ilk kurulum

--- api/server.js
+++ api/server.js
@@ -1,4 +1,16 @@
-const express = require('express')
-const server = express()
+const express = require('express');
+const server = express();
+
+const authRouter = require('./auth/auth-router');
+const ordersRouter = require('./orders/orders-router');
+const pizzasRouter = require('./pizzas/pizzas-router');
+const toppingRouter = require('./toppings/toppings-router');
+const usershRouter = require('./users/users-router');
+
+server.use('/api/auth',authRouter);
+server.use('/api/orders',ordersRouter);
+server.use('/api/pizzas',pizzasRouter);
+server.use('/api/toppings',toppingRouter);
+server.use('/api/users',usershRouter);
 
 module.exports = server;
