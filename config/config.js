e4811584cc2f5db4e5866e18683ede0189fb6f94
Bertan Sogutlu <bertansogutlu@gmail.com>
2023-04-14 16:50:58 +0300

    .env secret code guncellendi

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 PORT = process.env.PORT || 8000
 NODE_ENV = process.env.NODE_ENV
-JWT_SECRET = process.env.JWT_SECRET || "hede"
+JWT_SECRET = process.env.SECRET || "hede"
 
 
 
