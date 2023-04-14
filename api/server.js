054ce95fb2c9c37e7a49e5bb6c214f4ad80525c8
Bertan Sogutlu <bertansogutlu@gmail.com>
2023-04-14 14:30:04 +0300

    gecerli token server.js de kontrol ediliyor

--- api/server.js
+++ api/server.js
@@ -1,21 +1,24 @@
-const express = require('express');
+const express = require("express");
 const server = express();
 server.use(express.json());
+const { checkUserToken } = require("../utils/utils");
 
-const authRouter = require('./auth/auth-router');
-const ordersRouter = require('./orders/orders-router');
-const pizzasRouter = require('./pizzas/pizzas-router');
-const toppingRouter = require('./toppings/toppings-router');
-const usershRouter = require('./users/users-router');
+const authRouter = require("./auth/auth-router");
+const ordersRouter = require("./orders/orders-router");
+const pizzasRouter = require("./pizzas/pizzas-router");
+const toppingRouter = require("./toppings/toppings-router");
+const usershRouter = require("./users/users-router");
 
-server.use('/api/auth',authRouter);
-server.use('/api/orders',ordersRouter);
-server.use('/api/pizzas',pizzasRouter);
-server.use('/api/toppings',toppingRouter);
-server.use('/api/users',usershRouter);
+server.use("/api/auth", authRouter);
+server.use("/api/orders", checkUserToken, ordersRouter);
+server.use("/api/pizzas", checkUserToken, pizzasRouter);
+server.use("/api/toppings", checkUserToken, toppingRouter);
+server.use("/api/users", checkUserToken, usershRouter);
 
-// server.use((err,req,res,next)=>{
-//     res.status(err.status || 500).json({message: err.message || "Server error"})
-// })
+server.use((err, req, res, next) => {
+  res
+    .status(err.status || 500)
+    .json({ message: err.message || "Server error" });
+});
 
 module.exports = server;
