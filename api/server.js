3d5b8890a9679909578cffbf31e33cfd3aa6cfe2
Bertan Sogutlu <bertansogutlu@gmail.com>
2023-04-14 13:14:52 +0300

    genel hata mesaji server.js dosyasina eklendi

--- api/server.js
+++ api/server.js
@@ -14,4 +14,8 @@ server.use('/api/pizzas',pizzasRouter);
 server.use('/api/toppings',toppingRouter);
 server.use('/api/users',usershRouter);
 
+server.use((err,req,res,next)=>{
+    res.status(err.status || 500).json({message: err.message || "Server error"})
+})
+
 module.exports = server;
